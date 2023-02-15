7137ee273af7198d629ac1a049b8968e677b4438
tintesancristiana <cristianatintesan@gmail.com>
2023-02-15 10:23:03 +0200

    fixed the tests

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -64,8 +64,8 @@ class RecipeSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         """Create a recipe."""
         tags = validated_data.pop('tags', [])
-        recipe = Recipe.objects.create(**validated_data)
         ingredients = validated_data.pop('ingredients', [])
+        recipe = Recipe.objects.create(**validated_data)
         self._get_or_create_tags(
             tags,
             recipe
